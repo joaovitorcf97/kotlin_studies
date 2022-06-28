c17ab2768e57cc5ceb8cde981d562cbc75ab674c
Joao Vitor <joaovitorcfdev@gmail.com>
2022-06-28 19:59:12 -0300

    exercicio basico

---
+++ src/main/kotlin/basico/Exercicio.kt
@@ -0,0 +1,12 @@
+package basico
+
+fun main() {
+    var nomeCompleto: String = "João Vitor Costa Ferreira"
+    var texto: String
+    val numeroDoCalçado: Byte = 42
+    val pibBrasil: Long = 1869000000000
+    val populacaoDoBrasil: Int = 211000000
+
+    println("R$ ${pibBrasil / populacaoDoBrasil}")
+
+}
