545e31d5eb8e2e05dd4b1e6f8a9d040ddac9de07
Joao Vitor <joaovitorcfdev@gmail.com>
2022-06-28 19:36:16 -0300

    formatação de strings

---
+++ src/main/kotlin/basico/FormatacaoDeStrings.kt
@@ -0,0 +1,14 @@
+package basico
+
+fun main() {
+    val nome = "Tony"
+    val sobrenome = "Stark"
+
+    println("Ola, $nome $sobrenome")
+
+    println("Double: ${Double.MAX_VALUE}")
+
+    var str = "fdgfghdfgdf" +
+            "dfgfdgdfgdf" +
+            "fdgdfgdfgdf"
+}
